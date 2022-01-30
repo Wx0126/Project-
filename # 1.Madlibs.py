a86eade40093dce32b2e18dfab49de1f82a83027
Wx0126 <94050546+Wx0126@users.noreply.github.com>
2022-01-30 16:38:16 +0000

    Add files via upload

---
+++ # 1.Madlibs.py
@@ -0,0 +1,28 @@
+# 1.Madlibs
+# 2.Guess the Number (Computer)
+# 3.Guess the Number (user)
+# 4.Rock Paper Scissors
+# 5.Hangman
+# 6.Tic-Tac-Toe
+# 7.Tic-Tac-Toe AI
+# 8.Binary Search
+# 9.Minesweeper
+# 10.Sudoku Solver
+# 11.Photo Processing
+# 12.Markov Chain Composer
+
+
+
+
+# 1.Madlibs
+adj = input('adj: ')
+name = input('name: ')
+place = input('place:')
+adj = input('adj:')
+name = input('name:')
+
+
+
+madlib = f'My boyfriend is so {adj}! He loves to go to the gorcery with {name} together, \
+after that, he would go back to {place} and make a {adj} meal for {name}!'
+print(madlib)
